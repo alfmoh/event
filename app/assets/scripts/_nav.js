abe30cea7210780fe900855f248d801c312201de
alf <alfmohenu@outlook.com>
2018-07-09 12:15:13 +0300

    nav for mobile implemented. postcss media queries added.

--- app/assets/scripts/_nav.js
+++ app/assets/scripts/_nav.js
@@ -7,6 +7,10 @@ class Nav {
         this.navSpeakers = $("#nav-speakers");
         this.navFaq = $("#nav-faq");
 
+        this.menu = $(".site-nav-list-menu");
+        this.list = $(".site-nav-list");
+        this.listItem = $(".site-nav-list__item");
+
         this.events();
     }
 
@@ -15,6 +19,13 @@ class Nav {
         this.navAgenda.click(e => this.animator(e.currentTarget.id));
         this.navSpeakers.click(e => this.animator(e.currentTarget.id));
         this.navFaq.click(e => this.animator(e.currentTarget.id));
+
+        this.menu.click(this.toggleMenu.bind(this));
+    }
+
+    toggleMenu() {
+        this.list.toggleClass("site-nav-list-open");
+        this.listItem.toggleClass("site-nav-list__item-open");
     }
 
     animator(id) {
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,14 @@ const gulp = require("gulp"),
   autoprefixer = require("autoprefixer"),
   cssvars = require("postcss-simple-vars"),
   nested = require("postcss-nested"),
-  cssImport = require("postcss-import");
+  cssImport = require("postcss-import"),
+  mixins = require("postcss-mixins"),
+  customMedia = require("postcss-custom-media");
 
 gulp.task("styles", function() {
   return gulp
     .src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer, customMedia]))
     .on("error", function(err) {
       console.log(err.toString());
       this.emit("end");
